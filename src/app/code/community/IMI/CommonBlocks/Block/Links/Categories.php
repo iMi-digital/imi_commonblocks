805231647ac2383105fc42ce81318c9504ac7ef3
Alexander Menk <a.menk@imi.de>
2013-06-13 18:54:11 +0200

    Feature: #237 - Fetching list implemented

--- src/app/code/community/IMI/CommonBlocks/Block/Links/Categories.php
+++ src/app/code/community/IMI/CommonBlocks/Block/Links/Categories.php
@@ -25,14 +25,41 @@
  */
 class IMI_CommonBlocks_Block_Links_Categories extends Mage_Page_Block_Template_Links
 {
+    public function getParentCategoryId()
+    {
+        return 3; // "Vileda/Products"
+    }
+
+    protected function _construct()
+    {
+        parent::_construct();
+
+        /** same caching policy as in @see \Mage_Catalog_Block_Navigation::_construct */
+        $this->addData(array('cache_lifetime' => false));
+        $this->addCacheTag(array(
+            Mage_Catalog_Model_Category::CACHE_TAG,
+            Mage_Core_Model_Store_Group::CACHE_TAG
+        ));
+    }
+
+    public function getSubCategories()
+    {
+        $helper = Mage::helper('catalog/category');
+        $all = $helper->getStoreCategories();
+        echo count($all);
+    }
 
     protected function _prepareLayout()
     {
         parent::_prepareLayout();
 
-        $this->addLink('Mocked Category 1', 'link-mock1');
-        $this->addLink('Mocked Category 2', 'link-mock2');
+        $category = Mage::getModel('catalog/category');
+        $category->load($this->getParentCategoryId());
+        $children = $category->getChildrenCategories();
 
+        foreach($children as $child)  { /* @var $child Mage_Catalog_Model_Category */
+            $this->addLink($child->getName(), $child->getUrl());
+        }
         return $this;
     }
 }
