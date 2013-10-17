904c74fa507abd9c85318fd7002d91ec82a179cd
Stephan Nachtsheim <snm@development.imi.local>
2013-10-17 16:02:08 +0200

    * #2339 prepare reseller logo

---
+++ src/app/code/community/IMI/CommonBlocks/Helper/Reseller.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * iMi Magento Module
+ *
+ * NOTICE OF LICENSE
+
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * which is available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @copyright  Copyright (c) 2013 iMi digital GmbH (http://www.iMi.de)
+ * @author iMi digital GmbH <info@iMi.de>
+ * @license OSL-3.0
+ * @category IMI
+ * @package IMI_CommonBlocks
+ */
+
+
+class IMI_CommonBlocks_Helper_Reseller extends Mage_Core_Helper_Abstract
+{
+
+    /**
+     * Get logo image from URL
+     * @param $url string Reseller URL
+     */
+    public function getResellerLogo($url)
+    {
+        return false;
+    }
+}
